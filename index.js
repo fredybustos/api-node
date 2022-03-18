4710eb57f83ac1a5700e5e463494b0e43b11b101
fredy bustos <afredybustos@gmail.com>
2022-03-17 20:33:17 -0500

    added Procfile

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ const cors = require('cors')
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 app.use(express.json())
 
 routerApi(app)
 
-const whiteList = ['https://localhost:3000', 'https://myapp.com']
+const whiteList = ['http://localhost:3000', 'https://myapp.com']
 const corsOptions = {
   origin: (origin, callback) => {
-    if (whiteList.includes(origin)) {
+    if (whiteList.includes(origin) || !origin) {
       callback(null, true)
     }
     callback(new Error('Not allowed by CORS'))
@@ -24,6 +24,11 @@ app.use(cors(corsOptions))
 app.use(logErrors)
 app.use(boomErrorHandler)
 app.use(errorHandler)
+
+
+app.get('/', (req, res) => {
+  res.send('Hola mi server en express');
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
